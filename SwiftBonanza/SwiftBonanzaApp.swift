e5fe439ad10fd77248834494483044d2a5c8577a
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-08-26 12:05:36 +0500

    Initial Commit

---
+++ SwiftBonanza/SwiftBonanzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBonanzaApp.swift
+//  SwiftBonanza
+//
+//  Created by Николай Щербаков on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBonanzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
